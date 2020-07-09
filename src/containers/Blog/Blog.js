d60b1f959825082378d64d3a88f80ddd2fbb34bb
David Pam <niratpam@gmail.com>
2020-07-10 00:18:25 +0100

    Rendering fetched data to screen

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,14 +7,25 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then((response) => {
+      this.setState({ posts: response.data });
+      // console.log(response);
+    });
+  }
+
   render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post key={post.id} title={post.title} />;
+    });
+
     return (
       <div>
-        <section className='Posts'>
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className='Posts'>{posts}</section>
         <section>
           <FullPost />
         </section>
