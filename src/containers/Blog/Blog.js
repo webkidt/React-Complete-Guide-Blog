a07efd71bf3b33378eb637a28ce981190699652f
David Pam <niratpam@gmail.com>
2020-07-09 02:46:54 +0100

    Formatted starter code, Installed Axios, Added workspace settings

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,23 +6,23 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <section className='Posts'>
+          <Post />
+          <Post />
+          <Post />
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
