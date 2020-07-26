34c45f3870b80fdf5a86c9f9b19b0a8df00a18d7
David Pam <niratpam@gmail.com>
2020-07-26 03:19:15 +0100

    Setting up the Router Package

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import axios from '../../axios';
 
 import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -64,12 +62,6 @@ class Blog extends Component {
           </nav>
         </header>
         <section className='Posts'>{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
       </div>
     );
   }
